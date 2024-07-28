67e533fccf1e89484b80c2432e85c84272610dfb
jean.perea <jean.perea@rappi.com>
2024-07-28 17:15:10 -0500

    Primeros cambios

---
+++ formvalidations/build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    alias(libs.plugins.multiplatform)
+    alias(libs.plugins.android.library)
+    id("convention.publication")
+}
+
+group = "pe.devs.kmp.formvalidations"
+version = "1.0"
+
+kotlin {
+    androidTarget {
+        publishLibraryVariants("release")
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "17"
+            }
+        }
+    }
+
+    jvm()
+
+    js {
+        browser {
+            webpackTask {
+                mainOutputFileName = "formvalidations.js"
+            }
+        }
+        binaries.executable()
+    }
+
+    wasmJs {
+        browser()
+        binaries.executable()
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "formvalidations"
+            isStatic = true
+        }
+    }
+
+    listOf(
+        macosX64(),
+        macosArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "formvalidations"
+            isStatic = true
+        }
+    }
+
+    linuxX64 {
+        binaries.staticLib {
+            baseName = "formvalidations"
+        }
+    }
+
+
+    mingwX64 {
+        binaries.staticLib {
+            baseName = "formvalidations"
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+
+    }
+
+    //https://kotlinlang.org/docs/native-objc-interop.html#export-of-kdoc-comments-to-generated-objective-c-headers
+    targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget> {
+        compilations["main"].compilerOptions.options.freeCompilerArgs.add("-Xexport-kdoc")
+    }
+
+}
+
+android {
+    namespace = "pe.devs.kmp.formvalidations"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "Form-and-validations"
+include(":formvalidations")
+includeBuild("convention-plugins")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
